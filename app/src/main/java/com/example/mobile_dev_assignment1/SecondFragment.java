e6bcb46c27b3676cc13475e2e3659ed5f242ceb4
kinjals0130 <73653330+kinjals0130@users.noreply.github.com>
2023-10-04 21:58:50 -0400

    Updated first and second fragments to include bundles and display value.

--- app/src/main/java/com/example/mobile_dev_assignment1/SecondFragment.java
+++ app/src/main/java/com/example/mobile_dev_assignment1/SecondFragment.java
@@ -1,5 +1,7 @@
 package com.example.mobile_dev_assignment1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +13,6 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.mobile_dev_assignment1.databinding.FragmentSecondBinding;
-
-import org.w3c.dom.Text;
 
 public class SecondFragment extends Fragment {
 
@@ -42,6 +42,16 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+        //create a button that has an intent to go to the website that will give more info on EMI's
+        view.findViewById(R.id.btn_moreInfo).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.investopedia.com/terms/e/equated_monthly_installment.asp"));
+                startActivity(intent);
             }
         });
     }
